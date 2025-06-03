db03e2d1cb1b59d8f21d21d9fb51ddae054c0907
Bright Tier <thebrighttier@gmail.com>
2025-06-03 21:20:36 +0000

    y

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
+ allowedDevOrigins: ['https://7000-firebase-studio-1747828800055.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev'],
+
+
     ignoreBuildErrors: true,
   },
   eslint: {
